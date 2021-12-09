3816be0b25d94d14a60e428ea1ca0a4d12a36ef1
Cleo Menezes Jr <produtor.junior.menezes@gmail.com>
2021-12-09 17:57:08 -0300

    Chore: Green header also in dark mode.

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-const getClass = document.getElementsByClassName("_1XkO3")[0]
-getClass.style.width = "100vw";
-getClass.style.height = "98vh";
+const changeSize = document.getElementsByClassName("_1XkO3")[0];
+changeSize.style.width = "100vw";
+changeSize.style.height = "98vh";
+
+const changeBg = document.querySelector('.dark');
+if (changeBg != null) {
+    changeBg.style.setProperty('--app-background', '#009688');
+}
