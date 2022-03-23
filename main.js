33df144af6a140780ea42b9cb7710a5232c7ade6
Cleo Menezes Jr <produtor.junior.menezes@gmail.com>
2022-03-23 18:51:38 -0300

    fix: #1

--- main.js
+++ main.js
@@ -1,57 +1,54 @@
 const checkExist = setInterval(() => {
     const bodyApp = document.querySelector("._1XkO3");
     const headerIconsContainer = document.querySelector("._1QVfy._3UaCz");
-    // const chatContainer = document.querySelector('._3K4-L');
-    
+
     if (bodyApp != null) {
         bodyApp.id = 'maximize-wpp';
         headerIconsContainer.classList.add('config-container');
-        
-        
+
         // toggle button
-        const newDiv = document.createElement("div")
-        const newButton = document.createElement("a")
-        newButton.onclick = () => maximize(); 
-        newButton.style = 'padding: 8px 16px 8px 8px; display: flex; justify-items: center; cursor: pointer;'
-        newButton.setAttribute("type", "button")
-        newButton.setAttribute("class", "maximize-wpp-main-button")
-        const newImage = document.createElement("img")
-        newImage.setAttribute("src", "https://github.com/CleoMenezesJr/fullscreen-wpp/blob/main/icons/maximize-whatsapp-48.png?raw=true")
-        newImage.setAttribute("height", "24px")
-        headerIconsContainer.insertBefore(newDiv, headerIconsContainer.children[0]).appendChild(newButton).appendChild(newImage)
+        const newDiv = document.createElement("div");
+        const newButton = document.createElement("a");
+        newButton.onclick = () => maximize();
+        newButton.style = 'padding: 8px 16px 8px 8px; display: flex; justify-items: center; cursor: pointer;';
+        newButton.setAttribute("type", "button");
+        newButton.setAttribute("class", "maximize-wpp-main-button");
+        const newImage = document.createElement("img");
+        newImage.setAttribute("src", "https://github.com/CleoMenezesJr/fullscreen-wpp/blob/main/icons/maximize-whatsapp-48.png?raw=true");
+        newImage.setAttribute("height", "24px");
+        headerIconsContainer.insertBefore(newDiv, headerIconsContainer.children[0]).appendChild(newButton).appendChild(newImage);
 
 
         maximize();
         clearInterval(checkExist);
-    }
+    };
 }, 100);
 
 
 function maximize() {
     var maximizeWppId = document.querySelector("._1XkO3");
-    maximizeWppId
-        switch (maximizeWppId.id) {
-            case 'maximize-wpp':
-                maximizeWppId.id = 'maximize-wpp-on';
-                break;
-            case 'maximize-wpp-on':
-                maximizeWppId.id = 'maximize-wpp-off';
-                break;
-            case 'maximize-wpp-off':
-                maximizeWppId.id = 'maximize-wpp-on';
-                break;
-            default:
-                break;
-        }
-}
+    switch (maximizeWppId.id) {
+        case 'maximize-wpp':
+            maximizeWppId.id = 'maximize-wpp-on';
+            break;
+        case 'maximize-wpp-on':
+            maximizeWppId.id = 'maximize-wpp-off';
+            break;
+        case 'maximize-wpp-off':
+            maximizeWppId.id = 'maximize-wpp-on';
+            break;
+        default:
+            break;
+    };
+};
 
 
 const style = document.createElement('style');
 document.head.appendChild(style);
 style.sheet.insertRule(`
-    #maximize-wpp-on {
+    #maximize-wpp-on, ._1iwk6 {
         width: 100% !important;
         height: 100% !important;
         position: unset !important;
     }
-`)
+`);
